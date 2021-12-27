3521244ac4851f8d79bae877b029be001ee0a4a4
Your Name <you@example.com>
2021-12-27 21:16:04 -0200

    ok

--- admin.py
+++ admin.py
@@ -23,8 +23,7 @@ def procurar_usuario(chat_id):
 		cursor.execute(f"SELECT saldo, recargas, gifts, compras, usuario FROM usuarios WHERE chat_id = {chat_id}")
 		for s in cursor.fetchall():
 			...
-		print(s)
-		return s, s, s, s, s
+		return s[0], s[1], s[2], s, s
 @bot.message_handler(commands=['send'])
 def notificar(message):
   if verificar_admin(message.from_user.id) == True:
