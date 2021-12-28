e476d29f3dd92a86cbedcfb02c1bbabf2a3be6d9
Your Name <you@example.com>
2021-12-28 21:16:45 -0200

    ok

--- bot.py
+++ bot.py
@@ -29,8 +29,9 @@ crossIcon = u"\u274C"
 def makeKeyboard():
     markup = types.InlineKeyboardMarkup()
     for value in v:
+      print(value)
       markup.add(types.InlineKeyboardButton(text=value,callback_data="value"))
-        
+     
     return markup
 
 @bot.message_handler(commands=['test'])
