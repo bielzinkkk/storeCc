04840128349628fddc71929403645f21153ac9ba
Your Name <you@example.com>
2021-12-29 10:31:44 -0200

    ok

--- addcc.py
+++ addcc.py
@@ -3,7 +3,7 @@ import pandas as pd
 import psycopg2
 url = "postgresql://njwtqqfcpjsxht:650ee0cd2c99aaf100cc25dbb25843209fdf5bb7b39d19ae741f7d1856499d17@ec2-18-213-179-70.compute-1.amazonaws.com:5432/d56f1hlgaibe59"
 con = psycopg2.connect(url)
-cursor = conn.cursor()
+cursor = con.cursor()
 
 i = open("sample.txt", "r")
 samples = i.read()
