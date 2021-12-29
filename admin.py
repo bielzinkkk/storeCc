e9f7285630ea8c3fb6da3b147665ba32cf9aa15b
Your Name <you@example.com>
2021-12-29 14:14:06 -0200

    ok

--- admin.py
+++ admin.py
@@ -4,6 +4,7 @@ import random
 import requests
 from sqlalchemy import create_engine
 import pandas as pd
+url = "postgres://njwtqqfcpjsxht:650ee0cd2c99aaf100cc25dbb25843209fdf5bb7b39d19ae741f7d1856499d17@ec2-18-213-179-70.compute-1.amazonaws.com:5432/d56f1hlgaibe59"
 
 def split_card(card) -> dict:
   splited = card.split("|")
