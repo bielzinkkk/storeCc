aeb7eb73b756ad713177eff3708254d59da2a326
Your Name <you@example.com>
2021-12-29 21:49:54 -0200

    ok

--- cliente.py
+++ cliente.py
@@ -205,7 +205,7 @@ def back_menu(call):
 
 Olá</b> <a href='https://t.me/{call.from_user.username}'>{call.from_user.first_name}</a><b>, Seja bem vindo a store!</b>
 
-<a href='https://t.me/{userDono.split("@")}'>❓ Dúvidas</a>
+<a href='https://t.me/LORDEKG'>❓ Dúvidas</a>
 <a href='https://t.me/KINGST0RECHAT'>👥 Grupo</a>
 <a href='https://t.me/REFKG'>📣 Canal</a>
 <a href='https://t.me/Yusuke011'>⚙️ Dev</a>
