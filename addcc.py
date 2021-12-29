1b77fbc3adc3feaa2f5f47df5a6cb51b689eea8f
Your Name <you@example.com>
2021-12-29 12:22:10 -0200

    ok

--- addcc.py
+++ addcc.py
@@ -1,7 +1,7 @@
 import json 
 import pandas as pd
 import psycopg2
-from random import randiny
+
 url = "postgresql://njwtqqfcpjsxht:650ee0cd2c99aaf100cc25dbb25843209fdf5bb7b39d19ae741f7d1856499d17@ec2-18-213-179-70.compute-1.amazonaws.com:5432/d56f1hlgaibe59"
 con = psycopg2.connect(url)
 cursor = con.cursor()
