155310d6027d03d34afd02cf8402277f2fe7cb23
Your Name <you@example.com>
2021-12-29 11:32:08 -0200

    ok

--- addcc.py
+++ addcc.py
@@ -1,6 +1,7 @@
 import json 
 import pandas as pd
 import psycopg2
+import random
 url = "postgresql://njwtqqfcpjsxht:650ee0cd2c99aaf100cc25dbb25843209fdf5bb7b39d19ae741f7d1856499d17@ec2-18-213-179-70.compute-1.amazonaws.com:5432/d56f1hlgaibe59"
 con = psycopg2.connect(url)
 cursor = con.cursor()
@@ -27,6 +28,11 @@ for row in cards:
 print(cartao)
 from sqlalchemy import create_engine
 engine = create_engine(url)
-tabela = pd.DataFrame({"id": "BIGSERIAL PRIMARY KEY","cartao": cartao, "data": data, "cvv": cvv, "bin": '122', "banco": 'BANCO DO BRASIL', "nivel": 'PLATINUM', "tipo": 'CREDIT', "bandeira": 'VISA', "cpf": '282922', "nome": 'GABRIEL SANTOS'})
+result = []
+while len(result) != 4:
+    r = randint(0, 100)
+    if r not in result:
+        result.append(r)
+tabela = pd.DataFrame({"id": result,"cartao": cartao, "data": data, "cvv": cvv, "bin": '122', "banco": 'BANCO DO BRASIL', "nivel": 'PLATINUM', "tipo": 'CREDIT', "bandeira": 'VISA', "cpf": '282922', "nome": 'GABRIEL SANTOS'})
 tabela.to_sql(name='infocc', con=engine, if_exists='append', index=False)
 #ok
