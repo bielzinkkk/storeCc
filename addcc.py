b1cf8a8d155799b6c149bc4d9e161c2804439e4a
Your Name <you@example.com>
2021-12-29 09:56:58 -0200

    ok

--- addcc.py
+++ addcc.py
@@ -1,5 +1,6 @@
 import json 
 import pandas as pd
+import psycopg2
 url = "postgres://njwtqqfcpjsxht:650ee0cd2c99aaf100cc25dbb25843209fdf5bb7b39d19ae741f7d1856499d17@ec2-18-213-179-70.compute-1.amazonaws.com:5432/d56f1hlgaibe59"
 conn = psycopg2.connect(url)
 cursor = conn.cursor()
