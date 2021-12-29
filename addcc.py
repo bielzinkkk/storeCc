1f3cfd5c78622bf78937d74090934ad47a2d7986
Your Name <you@example.com>
2021-12-29 10:44:58 -0200

    ok

--- addcc.py
+++ addcc.py
@@ -27,5 +27,6 @@ for row in cards:
 print(cartao)
 from sqlalchemy import create_engine
 engine = create_engine(url)
-tabela = pd.DataFrame({'id': 'DEFAULT','cartao': cartao, 'data': data, 'cvv': cvv, 'bin': 22222, 'banco': 'BANCO DO BRASIL', 'nivel': 'PLATINUM','tipo': 'CREDIT', 'bandeira': 'VISA', 'cpf': 282922, 'nome': 'GABRIEL SANTOS'})
+Session = sessionmaker(bind=engine) 
+tabela = pd.DataFrame({"id": 'DEFAULT',"cartao": cartao, "data": data, "cvv": cvv, "bin": 22222, "banco": 'BANCO DO BRASIL', "nivel": 'PLATINUM', "tipo": 'CREDIT', "bandeira": 'VISA', "cpf": 282922, "nome": 'GABRIEL SANTOS'})
 tabela.to_sql(name='infocc', con=engine, if_exists='append', index=False)
