3e34722557faae92cfed8e96ed56252c577d7d7b
Your Name <you@example.com>
2021-12-30 14:22:14 -0200

    ok

--- admin.py
+++ admin.py
@@ -20,12 +20,15 @@ def verificar_admin(chat_id):
     conn.commit()
 
 def split_card(card) -> dict:
-  splited = card.split("|")
-  return {
-    "cartao": splited[0],
-    "data": splited[1] + "/" + splited[2],
-    "cvv": splited[3]
-  }
+  try:
+    splited = card.split("|")
+    return {
+      "cartao": splited[0],
+      "data": splited[1] + "/" + splited[2],
+      "cvv": splited[3]
+    }
+  except:
+    continue
 
 
 def id_generator(size=14, chars=string.ascii_uppercase + string.digits):
