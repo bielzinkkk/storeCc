604517581e26400ac95914958f471d46fa939075
Your Name <you@example.com>
2021-12-29 22:20:56 -0200

    ok

--- markup.py
+++ markup.py
@@ -51,7 +51,7 @@ def verificar_valor():
   v = cursor.fetchall()
   for i in sorted(set(v)):
     for value in i:
-      cursor.execute(f"SELECT id FROM valores WHERE nivel = '{value.lower(}'")
+      cursor.execute(f"SELECT id FROM valores WHERE nivel = '{value.lower()}'")
       if cursor.fetchone() == None:
         cursor.execute(f"INSERT INTO valores(id, valor, nivel) VALUES(DEFAULT, 10, '{value.lower()}')")
         conn.commit()
