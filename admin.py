045383db85322663bf383ab59ec8cbb8317975dc
Your Name <you@example.com>
2021-12-30 14:39:01 -0200

    ok

--- admin.py
+++ admin.py
@@ -75,7 +75,7 @@ def document(message):
 	      new_file.close()
 	    bot.send_message(message.chat.id, """Adicionando...""")
 	    f = open(path, "r")
-	    samples = f.readline()
+	    samples = [line for line in f.readlines() if line.strip()]
 	    cards = [split_card(card) for card in samples.strip().split("\n")]
 	    cartao = []
 	    data = []
