51176b0b764526a09fb408086b6824827e1cdcab
Your Name <you@example.com>
2022-01-02 14:04:32 -0200

    ok

--- cliente.py
+++ cliente.py
@@ -158,8 +158,7 @@ def unitariascall(call):
 *💳 | Estamos sem estoque no momento, volte mais tarde...*""", reply_markup=menuunitarias() ,parse_mode="MARKDOWN")
   else:
     	cursor.execute("SELECT nivel FROM infocc")
-    	for results in cursor.fetchall():
-    		...
+    	results = cursor.fetchall()
     	results_sorted = sorted([item[0] for item in results])
     	results_seted = set(results_sorted)
     	markups = generate_keyboard(list(results_seted),extra=InlineKeyboardButton(text="🔙 Voltar", callback_data="comprar"))
