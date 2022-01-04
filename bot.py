a47961f7246a7d75c764da6be83e94e762e8da8f
Your Name <you@example.com>
2022-01-04 00:43:36 -0200

    o

--- bot.py
+++ bot.py
@@ -20,3 +20,15 @@ cursor = conn.cursor()
 
 bot = telebot.TeleBot(token)
 import ast
+
+@bot.message_handler(commands=['gen'])
+def gen_command(message):
+	bot.send_message(message.chat.id, gen_full(), reply_markup=gen_markup, parse_mode="MARKDOWN")
+	
+@bot.callback_query_handler(func=lambda call: call.data == "trocar_dados")
+def trocardadoscall(call):
+	bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id, text=gen_full(), reply_markup=gen_markup,parse_mode="MARKDOWN")
+
+@bot.message_handler(commands=['chk'])
+def chk_command(message):
+	bot.send_message(message.chat.id, checker_full(), parse_mode="MARKDOWN")
--- ferramentas/ferramentas.py
+++ ferramentas/ferramentas.py
@@ -43,15 +43,3 @@ def gen_full():
 
 def checker_full(cartao):
 	return "*Está em manutenção*"
-
-@bot.message_handler(commands=['gen'])
-def gen_command(message):
-	bot.send_message(message.chat.id, gen_full(), reply_markup=gen_markup, parse_mode="MARKDOWN")
-	
-@bot.callback_query_handler(func=lambda call: call.data == "trocar_dados")
-def trocardadoscall(call):
-	bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id, text=gen_full(), reply_markup=gen_markup,parse_mode="MARKDOWN")
-
-@bot.message_handler(commands=['chk'])
-def chk_command(message):
-	bot.send_message(message.chat.id, checker_full(), parse_mode="MARKDOWN")
