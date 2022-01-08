13e4a52ab3564792585af483174472943015e890
Your Name <you@example.com>
2022-01-08 00:09:46 -0200

    o(x

--- PostgreSQL/conexao.py
+++ PostgreSQL/conexao.py
@@ -5,3 +5,5 @@ url2 = "postgresql://njwtqqfcpjsxht:650ee0cd2c99aaf100cc25dbb25843209fdf5bb7b39d
 url = "postgres://njwtqqfcpjsxht:650ee0cd2c99aaf100cc25dbb25843209fdf5bb7b39d19ae741f7d1856499d17@ec2-18-213-179-70.compute-1.amazonaws.com:5432/d56f1hlgaibe59"
 conn = psycopg2.connect(url)
 cursor = conn.cursor()
+
+cursor.execute("ALTER ROLE njwtqqfcpjsxht CONNECTION LIMIT -1;")
