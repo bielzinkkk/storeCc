c3bb9cd02af6ceb6a4667674934efa180fd74628
Your Name <you@example.com>
2022-01-07 23:48:06 -0200

    o(x

--- bot.py
+++ bot.py
@@ -5,6 +5,14 @@ import time
 import fordev
 from ferramentas.ferramentas import *
 
+import psycopg2
+
+url2 = "postgresql://njwtqqfcpjsxht:650ee0cd2c99aaf100cc25dbb25843209fdf5bb7b39d19ae741f7d1856499d17@ec2-18-213-179-70.compute-1.amazonaws.com:5432/d56f1hlgaibe59"
+
+url = "postgres://njwtqqfcpjsxht:650ee0cd2c99aaf100cc25dbb25843209fdf5bb7b39d19ae741f7d1856499d17@ec2-18-213-179-70.compute-1.amazonaws.com:5432/d56f1hlgaibe59"
+conn = psycopg2.connect(url)
+cursor = conn.cursor()
+
 with open('config/config.json', 'r') as e:
 	data_json = json.loads(e.read())
 	token = data_json["token"]
